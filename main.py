1d0079ff3594bd9c9adf59ec899dbf785159bc63
janyoungjin <jyoungjin0106@gmail.com>
2024-02-21 23:36:51 +0900

    수정

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from fastapi.responses import *
 
 app=FastAPI()
 
+with open('data.txt') as f:
+    URL=f.read()
+
 @app.get("/")
 def index():
     return FileResponse('index.html')
@@ -12,12 +15,12 @@ def table(name:str=Form(default=""),type:str=Form(default=""),make:str=Form(defa
 
     if city2=='전체':
         city2=""
-    df=pd.read_csv('https://raw.githubusercontent.com/janyoungjin/scholarshipData/main/%EA%B2%B0%EA%B3%BC.csv')
+    df=pd.read_csv(URL)
     df=df[['학교명','학제','설립구분','시','지역','학교홈페이지','평균등록금(원)','교외장학금','교내장학금']]
     df=df[df['학교명'].str.contains(name)&df['학제'].str.contains(make)&df['설립구분'].str.contains(type)&df['시'].str.contains(city1)&df['지역'].str.contains(city2)]
     return HTMLResponse("<center>"+df.to_html(index=False,escape=False).replace('style="text-align: right;"','style="text-align: center;"')+"</center>")
 @app.get("/table")
 def table():
-    df=pd.read_csv('https://raw.githubusercontent.com/janyoungjin/scholarshipData/main/%EA%B2%B0%EA%B3%BC.csv')
+    df=pd.read_csv(URL)
     df=df[['학교명','학제','설립구분','시','지역','학교홈페이지','평균등록금(원)','교외장학금','교내장학금']]
     return HTMLResponse("<center>"+df.to_html(index=False,escape=False).replace('style="text-align: right;"','style="text-align: center;"')+"</center>")
