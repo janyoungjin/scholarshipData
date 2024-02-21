fc595c5fc8e2fb8d61bc29e833dd70a20da42600
janyoungjin <jyoungjin0106@gmail.com>
2024-02-21 23:35:13 +0900

    pyscript에서 fastapi로 변경

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Form
+import pandas as pd
+from fastapi.responses import *
+
+app=FastAPI()
+
+@app.get("/")
+def index():
+    return FileResponse('index.html')
+@app.post("/table")
+def table(name:str=Form(default=""),type:str=Form(default=""),make:str=Form(default=""),city1:str=Form(default=""),city2:str=Form(default="")):
+
+    if city2=='전체':
+        city2=""
+    df=pd.read_csv('https://raw.githubusercontent.com/janyoungjin/scholarshipData/main/%EA%B2%B0%EA%B3%BC.csv')
+    df=df[['학교명','학제','설립구분','시','지역','학교홈페이지','평균등록금(원)','교외장학금','교내장학금']]
+    df=df[df['학교명'].str.contains(name)&df['학제'].str.contains(make)&df['설립구분'].str.contains(type)&df['시'].str.contains(city1)&df['지역'].str.contains(city2)]
+    return HTMLResponse("<center>"+df.to_html(index=False,escape=False).replace('style="text-align: right;"','style="text-align: center;"')+"</center>")
+@app.get("/table")
+def table():
+    df=pd.read_csv('https://raw.githubusercontent.com/janyoungjin/scholarshipData/main/%EA%B2%B0%EA%B3%BC.csv')
+    df=df[['학교명','학제','설립구분','시','지역','학교홈페이지','평균등록금(원)','교외장학금','교내장학금']]
+    return HTMLResponse("<center>"+df.to_html(index=False,escape=False).replace('style="text-align: right;"','style="text-align: center;"')+"</center>")
